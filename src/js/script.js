d37a219300f78b65a655b67a250f112718a4081b
Julia Abramova <julbeet@gmail.com>
2018-08-30 22:18:11 +0300

    initial commit

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+
+
+(function ($) {
+	'use strict';
+
+	$('.slider').slick({
+		dots: false,
+		// autoplay: true,
+		autoplaySpeed: 3000,
+		// infinite: true,
+		speed: 500,
+		fade: true,
+		cssEase: 'ease-in-out'
+	});
+
+
+
+
+})(jQuery)
