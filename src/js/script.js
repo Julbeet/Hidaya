afcd8f16c79d348b402dc50c5eb5e8a643155caf
Julia Abramova <julbeet@gmail.com>
2018-08-31 12:28:38 +0300

    fixed

--- src/js/script.js
+++ src/js/script.js
@@ -5,13 +5,17 @@
 
 	$('.slider').slick({
 		dots: false,
-		// autoplay: true,
-		autoplaySpeed: 3000,
-		// infinite: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		infinite: true,
 		speed: 500,
 		fade: true,
 		cssEase: 'ease-in-out'
 	});
+
+
+var picWidth = (($('.insta__picture')).width());
+$('#insta__box').css('width', picWidth );
 
 
 
